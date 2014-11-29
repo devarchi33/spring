08955534eb19ab3ad9ce3149947299c472e7c21f
이동훈 <skyfly33@iruen.com>
2014-11-29 23:28:45 +0900

    @SimpleDriverDataSouce를 javax.sql.Datasource 인터페이스로 변경.

--- src/test/java/spring05/user/service/UserServiceTest.java
+++ src/test/java/spring05/user/service/UserServiceTest.java
@@ -9,13 +9,14 @@ import static spring05.user.service.UserService.MIN_RECOMMEND_FOR_GOLD;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.sql.DataSource;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -40,7 +41,7 @@ public class UserServiceTest {
 	TestUtil testUtil;
 
 	@Autowired
-	SimpleDriverDataSource dataSource;
+	DataSource dataSource;
 
 	List<User> users;
 	User[] userArray = {
